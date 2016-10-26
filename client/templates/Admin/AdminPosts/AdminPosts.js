1a96e185f8c6f6bd654bad5372aa9d7284680842
pika298 <kit2008@live.com>
2016-10-27 00:13:35 +0800

    add functionality to edit/delete posts

--- client/templates/Admin/AdminPosts/AdminPosts.js
+++ client/templates/Admin/AdminPosts/AdminPosts.js
@@ -3,3 +3,12 @@ Template.AdminPosts.helpers({
 		return Posts.find();
 	}
 });
+
+Template.AdminPosts.events({
+	'click #deletePost': function () {
+		Posts.remove(this._id);
+	},
+	'click #editPost': function () {
+		Router.go('/admin/posts/'+this._id);
+	}
+});
